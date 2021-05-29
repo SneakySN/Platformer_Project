e6ed4b4f70624ecd5aed178530a3ffc2843a2dd3
Intae0099 <81225723+Intae0099@users.noreply.github.com>
2021-05-29 14:15:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,11 +23,190 @@ TOP_VIEWPORT_MARGIN = 100
 char_scaling = 0.25
 
 
-class MyGame(arcade.Window):
+class MainView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+        self.menu_list = None
+        self.menu_list = arcade.SpriteList(use_spatial_hash=True)
+
+        self.mouse_list = None
+        self.mouse_sprite = arcade.Sprite("pic/default/char.png", char_scaling)
+        self.remember_menu = 0
+
+        # 창에 마우스 보이게/안보이게 하는 기능
+        self.window.set_mouse_visible(False)
+
+    def menu_line(self, center_x, center_y, img):
+
+        menu = arcade.Sprite(img)
+        menu.center_x = center_x
+        menu.center_y = center_y
+        self.menu_list.append(menu)
+
+    def read(self):
+
+        self.menu_line(screen_w / 2, screen_h / 2 + 32 * 5, "pic/menu/start.png") # start
+        self.menu_line(screen_w / 2, screen_h / 2 + 32 * 2, "pic/menu/setting.png") # setting
+        self.menu_line(screen_w / 2, screen_h / 2 - 32 * 1, "pic/menu/mapedit.png") # map_edit
+        self.menu_line(screen_w / 2, screen_h / 2 - 32 * 4, "pic/menu/quit.png") # quit
+        self.menu_line(96, 32, "pic/menu/back.png") # back
+        self.mouse_list = arcade.SpriteList()
+        self.mouse_list.append(self.mouse_sprite)
+
+    def on_show(self):
+        arcade.set_background_color(arcade.csscolor.BLACK)
+        arcade.set_viewport(0, screen_w - 1, 0, screen_h - 1)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.read()
+        self.menu_list.draw()
+        self.mouse_list.draw()
+
+        arcade.draw_text("JUMPER", screen_w / 2, screen_h / 2 + 32 * 8, arcade.color.WHITE, font_size=30,
+                         anchor_x="center")
+
+    # 마우스로 화면 클릭시 게임화면으로 전환
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse_sprite.center_x = x
+        self.mouse_sprite.center_y = y
+
+    def on_mouse_press(self, _x, _y, button, _modifiers):
+
+        menu_hit_list = arcade.check_for_collision_with_list(self.mouse_sprite, self.menu_list)
+
+        for menu_index in menu_hit_list :
+
+            if button == arcade.MOUSE_BUTTON_LEFT :
+
+                if menu_index == self.menu_list[0]: # start
+                    game_view = GameView()
+                    game_view.setup()
+                    self.window.show_view(game_view)
+
+                if menu_index == self.menu_list[1]: # setting
+                    setting_view = SettingView()
+                    self.window.show_view(setting_view)
+
+                if menu_index == self.menu_list[2]: # map edit
+                    mapedit_view = MapeditView()
+                    self.window.show_view(mapedit_view)
+
+                if menu_index == self.menu_list[3]: # quit
+                    arcade.close_window()
+                    return 0
+
+                if menu_index == self.menu_list[4]: # back
+                    return 0
+
+class SettingView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+        self.menu_list = None
+        self.menu_list = arcade.SpriteList(use_spatial_hash=True)
+
+        self.mouse_list = None
+        self.mouse_sprite = arcade.Sprite("pic/default/char.png", char_scaling)
+        self.window.set_mouse_visible(False)
+
+    def menu_line(self, center_x, center_y, img):
+
+        menu = arcade.Sprite(img)
+        menu.center_x = center_x
+        menu.center_y = center_y
+        self.menu_list.append(menu)
+
+    def read(self):
+
+        self.menu_line(96, 32, "pic/menu/back.png")  # back
+        self.mouse_list = arcade.SpriteList()
+        self.mouse_list.append(self.mouse_sprite)
+
+    def on_show(self):
+        arcade.set_background_color(arcade.csscolor.BLACK)
+        arcade.set_viewport(0, screen_w - 1, 0, screen_h - 1)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.read()
+        self.menu_list.draw()
+        self.mouse_list.draw()
+
+        arcade.draw_text("JUMPER", screen_w / 2, screen_h / 2 + 32 * 8, arcade.color.WHITE, font_size=30,
+                         anchor_x="center")
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse_sprite.center_x = x
+        self.mouse_sprite.center_y = y
+
+    def on_mouse_press(self, _x, _y, button, _modifiers):
+
+        menu_hit_list = arcade.check_for_collision_with_list(self.mouse_sprite, self.menu_list)
+
+        for menu_index in menu_hit_list:
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                if menu_index == self.menu_list[0]:
+                    main_view = MainView()
+                    self.window.show_view(main_view)
+
+class MapeditView(arcade.View):
+
+    def __init__(self):
+        super().__init__()
+        self.menu_list = None
+        self.menu_list = arcade.SpriteList(use_spatial_hash=True)
+
+        self.mouse_list = None
+        self.mouse_sprite = arcade.Sprite("pic/default/char.png", char_scaling)
+        self.window.set_mouse_visible(False)
+
+    def menu_line(self, center_x, center_y, img):
+
+        menu = arcade.Sprite(img)
+        menu.center_x = center_x
+        menu.center_y = center_y
+        self.menu_list.append(menu)
+
+    def read(self):
+
+        self.menu_line(96, 32, "pic/menu/back.png")  # back
+        self.mouse_list = arcade.SpriteList()
+        self.mouse_list.append(self.mouse_sprite)
+
+    def on_show(self):
+        arcade.set_background_color(arcade.csscolor.BLACK)
+        arcade.set_viewport(0, screen_w - 1, 0, screen_h - 1)
+
+    def on_draw(self):
+        arcade.start_render()
+        self.read()
+        self.menu_list.draw()
+        self.mouse_list.draw()
+
+        arcade.draw_text("JUMPER", screen_w / 2, screen_h / 2 + 32 * 8, arcade.color.WHITE, font_size=30,
+                         anchor_x="center")
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse_sprite.center_x = x
+        self.mouse_sprite.center_y = y
+
+    def on_mouse_press(self, _x, _y, button, _modifiers):
+        menu_hit_list = arcade.check_for_collision_with_list(self.mouse_sprite, self.menu_list)
+
+        for menu_index in menu_hit_list:
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                if menu_index == self.menu_list[0]:
+                    main_view = MainView()
+                    self.window.show_view(main_view)
+
+
+class GameView(arcade.View):
     # Main application class.
     def __init__(self):
         # call the parent class and set up the window
-        super().__init__(screen_w, screen_h, screen_t)
+        super().__init__()
 
         arcade.set_background_color(arcade.csscolor.BLACK)
 
@@ -65,6 +244,7 @@ class MyGame(arcade.Window):
         self.save_char_y = None  # 플레이어 위치저장 변수
 
         self.cur_m = 0
+        self.window.set_mouse_visible(True)
 
     def map_read(self, map_file):
         with open(map_file, encoding='utf-8') as f_r:
@@ -95,7 +275,7 @@ class MyGame(arcade.Window):
             # coin
             for c in range(len(data["coins"])):
                 self.coin_line(data["coins"][c]["pos"][0], data["coins"][c]["pos"][1], data["coins"][c]["pos"][2],
-                                 data["coins"][c]["tex"], data["coins"][c]["place_direction"][0])
+                               data["coins"][c]["tex"], data["coins"][c]["place_direction"][0])
 
             # flag
             for e in range(len(data["flags"])):
@@ -135,14 +315,14 @@ class MyGame(arcade.Window):
             self.save_char_x = self.char_x
             self.save_char_y = self.char_y
 
-    def portal_line(self, start, end, height, img):  # end 대신에 num 변수를 통해 range 를 start*num으로 하는 것이 어떨지 제안.
+    def portal_line(self, start, end, height, img):
         for x in range(start, end, 32):
             portal = arcade.Sprite(img, char_scaling)
             portal.center_x = x
             portal.center_y = height
             self.portal_list.append(portal)
 
-    def wall_line(self, start, end, height, img, p_d):  # end 대신에 num 변수를 통해 range 를 start*num으로 하는 것이 어떨지 제안.
+    def wall_line(self, start, end, height, img, p_d):
         if p_d == "0":
             for x in range(start, end, 32):
                 wall = arcade.Sprite(img, char_scaling)
@@ -371,8 +551,9 @@ class MyGame(arcade.Window):
 
 def main():
     # Main method.
-    window = MyGame()
-    window.setup()
+    window = arcade.Window(screen_w, screen_h, screen_t)
+    start_view = MainView()
+    window.show_view(start_view)
     arcade.run()
 
 
